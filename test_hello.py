7cdb22715b12264d8b82a36106f052d88099593c
Ken Adachi <meatloaf111jp@gmail.com>
2021-05-07 11:02:41 +0000

    Fix test code:Fix wrong code of test class

--- test_hello.py
+++ test_hello.py
@@ -1,10 +1,19 @@
-def toyou(x):
-    return "hi %s" % x
+from hello import toyou, add, subtract
 
 
-def add(x):
-    return x + 1
+def setup_function(function):
+    print("Running Setup: %s" % function.__name__)
+    function.x = 10
 
 
-def subtract(x):
-    return x - 1
+def teardown_function(function):
+    print("Running Teardown: %s" % function.__name__)
+    del function.x
+
+
+### Run to see failed test
+#def test_hello_add():
+#    assert add(test_hello_add.x) == 12
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
